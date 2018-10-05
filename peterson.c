44e4d6b000b59d0b460cf9163ac0e675988b6f5c
15280095 <15280095@facin.portoalegre.pucrsnet.br>
2018-10-05 09:17:37 -0300

    Corrigido problemas de implementação peterson.c

--- peterson.c
+++ peterson.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 
-typedef struct peterson{
+struct Peterson{
     int *flag;
-    int turn = 0;
+    int turn;
+    int tamanhoVetor;
 }
 
-void unlock(struct *peterson, int i){
-    peterson->&flag[i]=0;
+void unlock (struct Peterson *peterson, int i){
+    peterson->flag[i]=0;
 }
 
-void lock(struct *peterson, int i){
-    peterson->&flag[i]=1;
-    turn = i;
-    int j = i-1;
-    if(j<0){
-        j = len(peterson->&flag)-1
-    }
-    while(peterson->&flag[j] && turn==i){
+void lock(struct Peterson *peterson, int i){
+    peterson->flag[i]=1;
+    peterson->turn = i;
+    int j = (i + (peterson->tamanhoVetor)-1) % (peterson->tamanhoVetor);
+    while(peterson->flag[j] && peterson->turn==i){
         continue;
     }
+}
+
+void main (){
+    struct Peterson peterson1;
+
+    peterson1.flag = (int*) malloc (sizeof (int) * 2);
+    peterson1.tamanhoVetor = 4;
+    peterson1.flag[1] = 10;
+
+    unlock(&peterson1, 1);
+
+    int j = (1 + (peterson1.tamanhoVetor)-1) % (peterson1.tamanhoVetor);
+    printf("%d",j);
 }
